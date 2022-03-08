6534f02beb19406ab5800bd963d989114963957a
Nils <nils.millot@canal-plus.com>
2022-03-08 23:47:19 +0100

    fix refactor private to admin home

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { UserContext } from "../../context/userContext";
 import { Outlet, Navigate } from "react-router-dom";
 
-export default function Private() {
+export default function Admin() {
   const { currentUser } = useContext(UserContext);
 
   if (!currentUser) {
--- src/pages/Admin/AdminHome/AdminHome.jsx
+++ src/pages/Admin/AdminHome/AdminHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AdminHome() {
-  return <div>PrivateHome</div>;
+  return <div>Admin Home</div>;
 }
