be54cda5a6581591d1e127ad4205bb61b31ffbac
Nils <nils.millot@canal-plus.com>
2022-03-08 14:46:49 +0100

    ad boiler plate for react-admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,15 +1,23 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/userContext";
 import { Outlet, Navigate } from "react-router-dom";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
 export default function Private() {
   const { currentUser } = useContext(UserContext);
+  const dataProvider = jsonServerProvider(
+    "https://jsonplaceholder.typicode.com"
+  );
 
   if (!currentUser) {
     return <Navigate to="/" />;
   }
   return (
     <>
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
       <Outlet />
     </>
   );
