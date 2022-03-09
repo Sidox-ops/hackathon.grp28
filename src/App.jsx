5bb3633b3d3793c8b2a89b1a853dea0f979255ab
Nils <nils.millot@canal-plus.com>
2022-03-09 18:56:17 +0100

    add cookies

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,22 @@ import StudiesServices from "./pages/StudiesServices";
 import ScientistValidation from "./pages/ScientistValidation";
 import AdminPanel from "./pages/Admin/AdminPanel/AdminPanel";
 import Admin from "./pages/Admin/Admin";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
     <>
+      <CookieConsent
+        location="bottom"
+        buttonText="Sure !!"
+        cookieName="acceptCookies"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        This website uses cookies to enhance the user experience.{" "}
+        <span style={{ fontSize: "10px" }}>Please accept it o:)</span>
+      </CookieConsent>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/WhatWeDo" element={<WhatWeDo />} />
