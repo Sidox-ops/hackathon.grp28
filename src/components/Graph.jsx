1adff5f91696a567a36ea8918632f2a0c82f8b1f
Sidox <pro.sidox@gmail.com>
2022-03-10 13:11:57 +0100

    chart for skc 1

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,49 +3,209 @@ import React from "react";
 import Result from "../data/data.json"
 
 import Chart from 'chart.js/auto'
-import {Line} from "react-chartjs-2"
+import { Line } from "react-chartjs-2"
 
-export default function Graph() {
+export default function GraphSKC1() {
     var sessions = [];
     var averages = [];
 
     Result.map((resultDetail, index) => {
-        if(!sessions.includes("T" + (resultDetail.session_id-1)) && resultDetail.session_id != ''){
-            sessions.push("T" + (resultDetail.session_id-1))
+        if (!sessions.includes("T" + (resultDetail.session_id - 1)) && resultDetail.session_id != '') {
+            sessions.push("T" + (resultDetail.session_id - 1))
         }
     })
 
-    console.log(sessions)
+    //1 no test 2 test
+
+    var product1_1 = [0, 0, 0, 0];
+    var product2_1 = [0, 0, 0, 0];
+
+    var product1_2 = [0, 0, 0, 0];
+    var product2_2 = [0, 0, 0, 0];
+
+
+    Result.map((resultDetail, index) => {
+
+
+        switch (resultDetail.product_code) {
+            case 417432:
+                if (resultDetail.zone_code == 1) {
+                    if (resultDetail.score_skinbiosense == 1) {
+                        switch (resultDetail.session_id) {
+                            case 1:
+                                product1_1[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+                            case 2:
+                                product1_1[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            case 3:
+                                product1_1[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+                            case 4:
+                                product1_1[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                } else if (resultDetail.zone_code == 2) {
+                    if (resultDetail.score_skinbiosense == 1) {
+                        switch (resultDetail.session_id) {
+                            case 1:
+                                product1_2[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+                            case 2:
+                                product1_2[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            case 3:
+                                product1_2[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+                            case 4:
+                                product1_2[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                break;
+            case 100218:
+                if (resultDetail.zone_code == 1) {
+                    if (resultDetail.score_skinbiosense == 1) {
+                        switch (resultDetail.session_id) {
+                            case 1:
+                                product2_1[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                break;
+                            case 2:
+                                product2_1[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                break;
+
+                            case 3:
+                                product2_1[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                break;
+                            case 4:
+                                product2_1[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                } else if (resultDetail.zone_code == 2) {
+                    if (resultDetail.score_skinbiosense == 1) {
+                        switch (resultDetail.session_id) {
+                            case 1:
+                                product2_2[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+                            case 2:
+                                product2_2[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            case 3:
+                                product2_2[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+                            case 4:
+                                product2_2[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    })
+
+
+
+    console.log("product 1", product1_1);
+    console.log("product 2", product2_1);
+    console.log("product 1", product1_2);
+    console.log("product 2", product2_2);
+    console.log(sessions);
     return (
-        <div style={{width:"550px", height:"450px"}}>
-            <Line
-                data={{ 
-                    labels: sessions,
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                }}
-                height={"25%"}
-                width={"35%"}
-            />
+        <div>
+            <div style={{ width: "650px", height: "550px", display: "inline-flex", margin: "40px" }}>
+                <Line
+                    data={{
+                        labels: sessions,
+                        datasets: [
+                            {
+                                label: 'No Product 417432',
+                                data: product1_1,
+                                backgroundColor: [
+                                    'rgba(54, 162, 235, 1)',
+
+                                ],
+                                borderColor: [
+                                    'rgba(54, 162, 235, 1)',
+
+                                ],
+                                borderWidth: 1
+                            },
+                            {
+                                label: 'No Product 100218',
+                                data: product2_1,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                ],
+                                borderWidth: 1
+                            },
+                            {
+                                label: 'Product 417432',
+                                data: product1_2,
+                                backgroundColor: [
+                                    'rgba(153, 102, 255, 1)',
+    
+                                ],
+                                borderColor: [
+                                    'rgba(153, 102, 255, 1)',
+    
+                                ],
+                                borderWidth: 1
+                            },
+                            {
+                                label: 'Product 100218',
+                                data: product2_2,
+                                backgroundColor: [
+                                    'rgba(75, 192, 192, 1)',
+                                ],
+                                borderColor: [
+                                    'rgba(75, 192, 192, 1)',
+                                ],
+                                borderWidth: 1
+                            }]
+                    }}
+                    height={"25%"}
+                    width={"35%"}
+                />
+            </div>
         </div>
     );
 }
