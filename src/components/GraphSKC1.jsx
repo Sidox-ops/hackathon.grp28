19fd370e8840cf22219f8e8290050c3244ad5b3d
Sidox <pro.sidox@gmail.com>
2022-03-10 17:02:40 +0100

    chart per user

--- src/components/GraphSKC1.jsx
+++ src/components/GraphSKC1.jsx
@@ -3,49 +3,49 @@ import React from "react";
 import Result from "../data/data.json"
 
 import Chart from 'chart.js/auto'
-import { Line } from "react-chartjs-2"
+import { Bar, Line } from "react-chartjs-2"
 
 export default function GraphSKC1() {
     var sessions = [];
 
+    //Définir les abcisses des graphes
     Result.map((resultDetail, index) => {
         if (!sessions.includes("T" + (resultDetail.session_id - 1)) && resultDetail.session_id != '') {
             sessions.push("T" + (resultDetail.session_id - 1))
         }
     })
 
-    //1 no test 2 test
-
+    // Definir les valeurs en ordonnés selon le score skinbiosense
     var product1_1 = [0, 0, 0, 0];
     var product2_1 = [0, 0, 0, 0];
 
     var product1_2 = [0, 0, 0, 0];
     var product2_2 = [0, 0, 0, 0];
-
-
     Result.map((resultDetail, index) => {
-
-
         switch (resultDetail.product_code) {
             case 417432:
+                // Fore each product Zone 1 no test product & Zone 2 test product
+
                 if (resultDetail.zone_code == 1) {
                     if (resultDetail.score_skinbiosense == 2) {
+
+                        // Zone de raisonnement un peu statique par rapport à ce dataset need to dynamiser
                         switch (resultDetail.session_id) {
                             case 1:
-                                product1_1[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_1[0] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
                             case 2:
-                                product1_1[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_1[1] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
                             case 3:
-                                product1_1[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_1[2] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
                             case 4:
-                                product1_1[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_1[3] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
@@ -57,20 +57,20 @@ export default function GraphSKC1() {
                     if (resultDetail.score_skinbiosense == 1) {
                         switch (resultDetail.session_id) {
                             case 1:
-                                product1_2[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_2[0] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
                             case 2:
-                                product1_2[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_2[1] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
                             case 3:
-                                product1_2[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_2[2] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
                             case 4:
-                                product1_2[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product1_2[3] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
@@ -86,17 +86,17 @@ export default function GraphSKC1() {
                     if (resultDetail.score_skinbiosense == 1) {
                         switch (resultDetail.session_id) {
                             case 1:
-                                product2_1[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_1[0] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
                                 break;
                             case 2:
-                                product2_1[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_1[1] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
                                 break;
 
                             case 3:
-                                product2_1[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_1[2] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
                                 break;
                             case 4:
-                                product2_1[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_1[3] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
@@ -108,20 +108,20 @@ export default function GraphSKC1() {
                     if (resultDetail.score_skinbiosense == 1) {
                         switch (resultDetail.session_id) {
                             case 1:
-                                product2_2[0] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_2[0] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
                             case 2:
-                                product2_2[1] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_2[1] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
                             case 3:
-                                product2_2[2] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_2[2] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
                             case 4:
-                                product2_2[3] += parseFloat(resultDetail.mesure.replace(',', '.'))/100
+                                product2_2[3] += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
 
                                 break;
 
@@ -135,16 +135,44 @@ export default function GraphSKC1() {
             default:
                 break;
         }
-
     })
 
+    // Remplir second graph pour pondérer le premier
+    var list_users = [];
+    var list_averages = [];
 
+    Result.map((resultDetail, index) => {
+        if (!list_users.includes(resultDetail.user_id)
+            && resultDetail.user_id != "zone_code"
+            && resultDetail.user_id != "score_skinbiosense"
+            && resultDetail.user_id != "product_code"
+            && resultDetail.user_id != "session_id") {
 
-    console.log("product 1", product1_1);
-    console.log("product 2", product2_1);
-    console.log("product 1", product1_2);
-    console.log("product 2", product2_2);
-    console.log(sessions);
+            list_users.push(resultDetail.user_id)
+
+        }
+    })
+
+    /*Result.map((resultDetail, index) => {
+        let average = 0;
+        //console.log(index)
+        if (resultDetail.user_id == list_users[index]) {
+            average += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
+            list_averages.push(average)
+        }
+    })*/
+
+    list_users.forEach(user => {
+        let average = 0;
+        Result.map((resultDetail, index) => {
+            if (resultDetail.user_id == user && resultDetail.score_skinbiosense == 1) {
+                average += parseFloat(resultDetail.mesure.replace(',', '.')) / 100
+            }
+        })
+        list_averages.push(average)
+    })
+
+    console.log("list", list_averages)
     return (
         <div>
             <div style={{ width: "650px", height: "550px", display: "inline-flex", margin: "40px" }}>
@@ -181,11 +209,11 @@ export default function GraphSKC1() {
                                 data: product1_2,
                                 backgroundColor: [
                                     'rgba(153, 102, 255, 1)',
-    
+
                                 ],
                                 borderColor: [
                                     'rgba(153, 102, 255, 1)',
-    
+
                                 ],
                                 borderWidth: 1
                             },
@@ -200,6 +228,37 @@ export default function GraphSKC1() {
                                 ],
                                 borderWidth: 1
                             }]
+                    }}
+                    height={"25%"}
+                    width={"35%"}
+                />
+                {/*
+                    second graphique pour pondérer les résultats affichés par le premier
+                */}
+                <Bar
+                    data={{
+                        labels: list_users,
+                        datasets: [{
+                            label: '# of Votes',
+                            data: list_averages,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
                     }}
                     height={"25%"}
                     width={"35%"}
