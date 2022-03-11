1e2a7dc514ef44abda1cc355bc543d7894c128a5
Sidox <pro.sidox@gmail.com>
2022-03-11 12:42:51 +0100

    fix after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Home from "./pages/Home";
 import WhatWeDo from "./pages/WhatWeDo";
 import OurSolutions from "./pages/OurSolutions";
@@ -9,16 +8,14 @@ import ScientistValidation from "./pages/ScientistValidation";
 import Dashboard from "./pages/Admin/Dashboard";
 import NotFound from "./pages/NotFound";
 import CookieConsent from "react-cookie-consent";
-=======
 // import Home from "./pages/Home";
 import GraphSKC1 from "./components/GraphSKC1";
 import GraphSKC2 from "./components/GraphSKC2";
 import GraphSKC3 from "./components/GraphSKC3";
 
 import Navbar from "./components/Navbar";
-import Private from "./pages/Private/Private";
-import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
->>>>>>> features/chart
+//import Private from "./pages/Private/Private";
+//import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
 
 function App() {
   return (
@@ -36,7 +33,6 @@ function App() {
       </CookieConsent>
 
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Home />} />
         <Route path="/WhatWeDo" element={<WhatWeDo />} />
         <Route path="/OurSolutions" element={<OurSolutions />} />
@@ -46,13 +42,11 @@ function App() {
 
         <Route path="/admin" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
-=======
         <Route path="/charts" element={<><GraphSKC1 /><GraphSKC2 /><GraphSKC3 /></>}  />
 
-        <Route path="/private" element={<Private />}>
+        {/*<Route path="/private" element={<Private />}>
           <Route path="/private/private-home" element={<PrivateHome />} />
-        </Route>
->>>>>>> features/chart
+        </Route>*/}
       </Routes>
     </>
   );
